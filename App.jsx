eed14c24626678cb4bbb01d9493a9955b452448a
Victormattos564 <victormaria@aluno.senai.br>
2024-02-21 09:55:58 -0300

    feat: addtask/removetask

--- App.jsx
+++ App.jsx
@@ -6,6 +6,20 @@ import React from 'react';
 export default function App() {
 
   const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+	  if (task.trim().length > 0) {
+	    setTasks([...tasks, { id: Math.random().toString(), value: task }]);
+	    setTask('')
+	  }
+	};
+
+  const removeTask = (id) => {
+    setTasks(tasks => {
+      return tasks.filter((task) => task.id !== id);
+    });
+  };
   
   return (
     <View style={styles.container}>
@@ -16,10 +30,28 @@ export default function App() {
       onChangeText={setTask}
       value={task}
     />
-    <TouchableOpacity style={styles.button}>
+    <TouchableOpacity style={styles.button}  onPress={addTask}>
       <Text style={styles.text}>Adicionar</Text>
     </TouchableOpacity>
   </View>
+  <View style={styles.container2}>
+  {tasks.length === 0 ? (
+    <Text style={styles.textRed}>Nenhuma Tarefa Cadastrada</Text>
+  ) : (
+    <Text style={styles.textGreen}>Tarefas Cadastradas</Text>
+  )}
+  {
+    tasks.map((task) => (
+      <View key={task.id} style={styles.form}>
+        <Text>{task.value}</Text>
+        <TouchableOpacity style={styles.button} onPress={removeTask.bind(this, task.id)}>
+          <Text style={styles.text}>Remover</Text>
+        </TouchableOpacity>
+      </View>
+    ))
+  
+  }
+</View>
   <StatusBar style="auto" />
 </View>
   );
@@ -44,10 +76,24 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: 'blue',
-    padding: 10,
+    padding: 2,
     borderRadius: 4,
   },
   text: {
     color: 'white',
+  },
+  container2: {
+    marginBottom: 6,
+    marginTop: 12,
+  },
+  textRed: {
+    textAlign: 'center',
+    color: 'red',
+    fontSize: 18,
+  },
+  textGreen: {
+    textAlign: 'center',
+    color: 'green',
+    fontSize: 18,
   },
 });
