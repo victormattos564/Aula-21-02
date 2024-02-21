ea8a198db03eb8c1350919f4236f3140a1fec59a
Victormattos564 <victormaria@aluno.senai.br>
2024-02-21 09:39:47 -0300

    feat: formulario de adiçao de tarefas

--- App.jsx
+++ App.jsx
@@ -1,19 +1,53 @@
-import { Text, View, StyleSheet } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import React from 'react';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
-    </View>
-	);
+
+  const [task, setTask] = useState('');
+  
+  return (
+    <View style={styles.container}>
+  <View style={styles.form}>
+    <TextInput
+      placeholder="Digite uma nova tarefa"
+      style={styles.inputline}
+      onChangeText={setTask}
+      value={task}
+    />
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.text}>Adicionar</Text>
+    </TouchableOpacity>
+  </View>
+  <StatusBar style="auto" />
+</View>
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 12,
+    marginTop: 24,
+  },
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 6,
+  },
+  inputline: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    padding: 2,
+    width: '75%',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 4,
+  },
+  text: {
+    color: 'white',
   },
 });
